29217c8d2e09f9dc1973d5e5ff4f317c7054a777
algernon-A <aeneas@aloku.net>
2022-07-25 10:46:37 +1000

    Add project files.

---
+++ Code/Mod.cs
@@ -0,0 +1,41 @@
+﻿using CitiesHarmony.API;
+using ICities;
+
+
+namespace CleanAnimalNames
+{
+    /// <summary>
+    /// The base mod class for instantiation by the game.
+    /// </summary>
+    public class Mod : IUserMod
+    {
+        public static string ModName => "Clean Animal Names";
+
+        public string Name => ModName + " " + ModUtils.CurrentVersion;
+        public string Description => "Cleans up the names of custom animal prefabs";
+
+
+        /// <summary>
+        /// Called by the game when the mod is enabled.
+        /// </summary>
+        public void OnEnabled()
+        {
+            // Apply Harmony patches via Cities Harmony.
+            // Called here instead of OnCreated to allow the auto-downloader to do its work prior to launch.
+            HarmonyHelper.DoOnHarmonyReady(() => Patcher.PatchAll());
+        }
+
+
+        /// <summary>
+        /// Called by the game when the mod is disabled.
+        /// </summary>
+        public void OnDisabled()
+        {
+            // Unapply Harmony patches via Cities Harmony.
+            if (HarmonyHelper.IsHarmonyInstalled)
+            {
+                Patcher.UnpatchAll();
+            }
+        }
+    }
+}
---
+++ Code/Patches/GenerateName.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+
+
+namespace CleanAnimalNames
+{
+    /// <summary>
+    /// Harmony patch to return cleaned-up names for custom animal prefabs.
+    /// </summary>
+    [HarmonyPatch(typeof(AnimalAI), "GenerateName")]
+    public static class GenerateName
+    {
+        /// <summary>
+        /// Harmony Postfix patch to AnimalAI.GenerateName to return cleaned-up name for custom prefabs.
+        /// </summary>
+        /// <param name="__instance">AI instance reference</param>
+        /// <param name="__result">Method result reference</param>
+        /// <returns>False (don't execute original method) if a cleaned-up name was generated, true (continue execution chain) otherwise</returns>
+        public static bool Prefix(AnimalAI __instance, ref string __result)
+        {
+            // Check for period in prefab name.
+            int periodPos = __instance.m_info.name.IndexOf('.');
+            if (periodPos >= 0)
+            {
+                // Found a period - strip off leading package ID and any trailing _Data and returned the cleaned-up name.
+                __result = __instance.m_info.name.Substring(periodPos + 1).Replace("_Data", "");
+                return false;
+            }
+            // No period - check for traiing _Data for local assets.
+            else if (__instance.m_info.name.EndsWith("_Data"))
+            {
+                // Trailing _Data found - trim and return the trimmed name.
+                __result = __instance.m_info.name.Substring(0, __instance.m_info.name.Length - 5);
+                return false;
+            }
+
+            // Not a custom asset - continue to game method.
+            return true;
+        }
+    }
+}
